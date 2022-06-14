e5e820d8d59af987059aafb18568c84216ea6cd1
lopo <lopo@zju.edu.cn>
2022-06-14 22:40:10 +0800

    temp: ...

--- lib/index.ts
+++ lib/index.ts
@@ -1,10 +1,20 @@
-// type OlController
-//
-// class OlController {
-//     constructor() {
-//     }
-// }
-//
-// export {
-//     OlController
-// }
+import { Map as OlMap } from "ol";
+import { OSM, XYZ } from "ol/source";
+
+
+
+class OlController {
+    /**
+     * @description container element in dom
+     * @private
+     */
+    #container: HTMLElement | null = null
+    #handle: OlMap | null = null
+
+    constructor(el: HTMLElement, src?: string) {
+    }
+}
+
+export {
+    OlController
+}
