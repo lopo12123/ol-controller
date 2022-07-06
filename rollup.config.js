cbe659449ffeb2e3f78945a119a302b409d96eaf
lopo <lopo@zju.edu.cn>
2022-07-06 14:22:59 +0800

    feat: build for esm/cjs

--- rollup.config.js
+++ rollup.config.js
@@ -6,34 +6,25 @@ import {terser} from "rollup-plugin-terser";
  * @type {import('rollup').RollupOptions}
  */
 export default defineConfig({
-    input: 'lib/index.ts',
-    output: [
-        // esm
-        {
-            file: 'dist/index.mjs',
-            format: 'esm',
-            sourcemap: 'inline'
-        },
-        // esm.min
-        {
-            file: 'dist/index.min.mjs',
-            format: 'esm',
-            sourcemap: 'inline',
-            plugins: [terser()]
-        },
-        // cjs
-        {
-            file: 'dist/index.js',
-            format: 'cjs',
-            sourcemap: 'inline',
-        },
-        // cjs.min
-        {
-            file: 'dist/index.min.js',
-            format: 'cjs',
-            sourcemap: 'inline',
-            plugins: [terser()]
-        }
+    input: [
+        'lib/core.ts',
+        'lib/animation.ts',
+        'lib/popup.ts',
+        'lib/index.ts'
     ],
-    plugins: [typescript({tsconfig: './tsconfig.lib.json'})],
+    output: [
+        {
+            dir: 'dist/esm',
+            format: 'esm',
+            sourcemap: 'inline',
+            plugins: [terser()]
+        },
+        // {
+        //     dir: 'dist/cjs',
+        //     format: 'cjs',
+        //     sourcemap: 'inline',
+        //     plugins: [terser()]
+        // },
+    ],
+    plugins: [typescript({tsconfig: './tsconfig.esm.json'})],
 })
